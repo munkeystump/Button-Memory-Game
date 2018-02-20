5efdb252e7ee7f9043c8ed709caed5fce4944b10
lookkeyescape <munkeystump@gmail.com>
2018-02-20 21:38:11 +0000

    Updated all code
    Original code was incomplete and took entirely different approach. This is the live version used in production

--- roses.ino
+++ roses.ino
@@ -1,51 +1,146 @@
-// Painting the roses red puzzle
-
-int button[] = {2,3,4,5,6}; //pins for buttons whiuch turn on lights
-int lights[] = {7,8,9,10,11}; // pins for light to turn on
-int rset = 13;
-int lock = 12; // pin controlling relay for 12V maglock
-int inputs = 5; // number of buttons/led combinations
-int complete = 0;
+#include <Key.h>
+#include <Keypad.h>
+#define  numLeds 12
 
 
-void setup() {
-  Serial.begin(9600);
-  for (int i = 0; i < inputs; i++){
-    pinMode(button[i], INPUT); // for all buttons which turns lights on set as input
-    pinMode(lights[i], OUTPUT); // for all leds set output
+const byte numRows= 4; //number of rows on the keypad
+const byte numCols= 5; //number of columns on the keypad
+
+//keymap defines the key pressed according to the row and columns just as appears on the keypad
+char keymap[numRows][numCols]=
+{
+{'A', 'B', '*', 'C','*'},
+{'D', '*', 'E', '*','F'},
+{'*', 'G', 'H', '*','I'},
+{'J', '*', '*', 'K','L'}
+};
+
+//Code that shows the the keypad connections to the arduino terminals
+byte rowPins[numRows] = {11,10,9,8}; //Rows 0 to 3
+byte colPins[numCols]= {6,5,4,3,2}; //Columns 0 to 3
+
+//initializes an instance of the Keypad class
+Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+//initialise led pins
+byte ledPins[] = {30,31,32,33,34,35,36,37,38,39,40,41};
+
+//pin for maglock
+int lock = 12;
+//int solved = 0;
+char solvedMap[numLeds];
+
+void setup()
+{
+Serial.begin(9600);
+  for (int i = 0 ; i < numLeds ; i++){
+    pinMode(ledPins[i],OUTPUT);
   }
-  pinMode(lock, OUTPUT); // set lock pin as output
-  pinMode(rset, INPUT); // set all other buttons to reset the puzzle (lights all off)
+  pinMode(lock,OUTPUT);
+  rset();
+  Serial.println(":: Painting the Roses Red Puzzle Initialised ::");
 }
 
-void loop() {
-  for (int i = 0; i < inputs; i++){       // scan each button which turns leds on to see if it has been pressed
-    if (digitalRead(button[i])== HIGH){
-      digitalWrite(lights[i], HIGH);
-      complete++;
+
+void loop()
+{
+
+char key = myKeypad.getKey();
+if(key)  // Check for a valid key.
+  {
+    Serial.print(key);    
+    switch (key)
+    {
+      case '*':
+        rset();
+        break;
+      case 'A':
+        digitalWrite(ledPins[0], LOW);
+        solvedMap[0]=key;
+        break;
+      case 'B':
+        digitalWrite(ledPins[1], HIGH);
+        solvedMap[1]=key;
+        break;
+      case 'C':
+        digitalWrite(ledPins[2], HIGH);
+        solvedMap[2]=key;
+        break;
+      case 'D':
+        digitalWrite(ledPins[3], HIGH);
+        solvedMap[3]=key;
+        break;
+      case 'E':
+        digitalWrite(ledPins[4], HIGH);
+        solvedMap[4]=key;
+        break;
+      case 'F':
+        digitalWrite(ledPins[5], HIGH);
+        solvedMap[5]=key;
+        break;
+      case 'G':
+        digitalWrite(ledPins[6], HIGH);
+        solvedMap[6]=key;
+        break;
+      case 'H':
+        digitalWrite(ledPins[7], HIGH);
+        solvedMap[7]=key;
+        break;
+      case 'I':
+        digitalWrite(ledPins[8], HIGH);
+        solvedMap[8]=key;
+        break;
+      case 'J':
+        digitalWrite(ledPins[9], HIGH);
+        solvedMap[9]=key;
+        break;
+      case 'K':
+        digitalWrite(ledPins[10], HIGH);
+        solvedMap[10]=key;
+        break;
+      case 'L':
+        digitalWrite(ledPins[11], HIGH);
+        solvedMap[11]=key;
+        break;
+      default:
+        Serial.println(key);
+    }
+    Serial.print(" - Key pressed :: Current Solved Status - ");
+    prsMap();
+    if (solved()){
+      unlock();
     }
   }
-  if (digitalRead(rset) == HIGH){         // if any other button is pressed turn off all leds
-    for (int j = 0; j < inputs; j++){
-      digitalWrite(lights[j], LOW);
-      complete = 0;
+}
+
+boolean solved(){
+  for (int x = 0 ; x < numLeds ; x ++){
+    if (solvedMap[x]=='X'){
+      return false;
     }
   }
-  if (complete == inputs){
-    unlock();
+  return true;
+}
+
+void rset(){
+  for (int x = 0 ; x < numLeds ; x ++){
+    solvedMap[x]='X';
+    digitalWrite(ledPins[x], HIGH);
   }
+}
+
+void prsMap(){
+  for (int x = 0 ; x < numLeds ; x ++){
+    Serial.print(solvedMap[x]);
+  }
+  Serial.println();
 }
 
 void unlock(){
-  digitalWrite(lock, HIGH);
-  for (int j = 0; j < inputs; j++){
-    digitalWrite(lights[j], LOW);
-    delay(50);
-  }
-  for (int j = 0; j < inputs; j++){
-    digitalWrite(lights[j], HIGH);
-    delay(50);
-  }
-  delay(100);
-  digitalWrite(lock, LOW);
+  Serial.println("Lock Open");
+  digitalWrite(lock,HIGH);
+  delay(10000);
+  digitalWrite(lock,LOW);
+  Serial.println("Lock Closed");
+  rset();
 }
